8eb3916fa14598429f80aed37ba4b0f0a0a3293c
Mohab Emad <Mohab Emad@MOHAB>
2016-05-25 21:16:42 +0200

    Add project files.

---
+++ linked_list/linked_list.h
@@ -0,0 +1,28 @@
+#pragma once
+template<class T> 
+class linked_list
+{
+public:
+	linked_list(T* obj,int size_objs);
+
+	~linked_list();
+
+	//----- Functions-----//
+	T operator[](int index);
+	int* find(T obj);
+	int size();
+	void add_node(T obj);
+	void delete_node(int index);
+	void clear();
+private:
+	int m_length ;
+	struct node
+	{
+		node * m_next;
+		T m_obj;
+	};
+	node* m_head;
+	node* m_tail;
+	typename node * node_at(int index);
+};
+#include"linked_list.cpp"
