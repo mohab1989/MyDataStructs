a66cdb7729e2d52508e313b70e61faa19f1108bb
mohab1989 <mohabe1989@gmail.com>
2016-05-26 18:41:50 +0200

    initial commit

--- linked_list/main.cpp
+++ linked_list/main.cpp
@@ -5,19 +5,9 @@
 void main(int argc, char * argv[])
 {
 	const int size_vals = 10;
-	int vals[size_vals]{ 0,1,2,3,4,5,6,7,8,9 };
+	int vals[size_vals]{ 0,1,2,3,4,2,2,7,8,2 };
 	linked_list<int> llist(&vals[0],size_vals);
-	llist.add_node(10);
-	llist.delete_node(3);
-	int i = llist[2];
-	 i = llist[3];
-	 i = llist[4];
-	 llist.delete_node(0);
-	 i = llist[0];
-	 i = llist[1];
-	 llist.delete_node(llist.size() - 1);
-	 i = llist[llist.size() - 2];
-	 i = llist[llist.size() - 1];
-	 llist.clear();
+	std::vector<int> indicies= llist.find(2);
+	llist.clear();
 	system("pause");
 }
